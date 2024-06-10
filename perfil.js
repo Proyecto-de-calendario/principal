a458b5dce225d9a129014b7e0bcd06a7cadc6ba3
OcampoEmiliano <ocampoemilianopa@gmail.com>
2024-06-10 16:43:58 -0300

    styles agregado

--- perfil.js
+++ perfil.js
@@ -4,6 +4,6 @@ function actualizar() {
 function actualizarPerfil() {
     let elActualizar = document.getElementById("actualizar");
     elActualizar.addEventListener("click", (e)=> {
-      document.
+      
     })
   }
