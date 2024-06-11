ecabdb3c0dba06982f68261a94996b6f2cecc601
OcampoEmiliano <ocampoemilianopa@gmail.com>
2024-06-11 16:23:40 -0300

    perfil con estilo

--- perfil.js
+++ perfil.js
@@ -1,9 +1,21 @@
 function actualizar() {
-
+let actualizarPerfil = document.getElementById("actualizar-perfil");
+actualizarPerfil.addEventListener("btn")
 }
 function actualizarPerfil() {
-    let elActualizar = document.getElementById("actualizar");
-    elActualizar.addEventListener("click", (e)=> {
-      
+    let formulario = document.getElementById("formulario");
+    
+      formulario.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        let nuevoNombre = document.getElementById("nombre").value;
+        let nuevoEmail = document.getElementById("email").value;
+        let nuevaedad = document.getElementById("edad").value;
+
+        localStorage.setItem("nombre", nuevoNombre);
+        localStorage.setItem("email", nuevoEmail);
+        localStorage.setItem("edad", nuevaedad);
+
+        console.log("¡Datos guardados!");
     })
   }
