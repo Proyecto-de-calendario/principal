941a9c3778ee2ab2618dc2c1a4474e865cf1e8bf
OcampoEmiliano <167482441+OcampoEmiliano@users.noreply.github.com>
2024-07-26 10:31:45 -0300

    validaciones del server

--- scr/app.js
+++ scr/app.js
@@ -7,8 +7,8 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 // Modulo de rutas - Todos los endpoints estarían aquí
-const userRouter = require('./routes/users');
-const taskRouter = require('./routes/tasks'); 
+const userRouter = require('./routes/usuarios.Routes');
+const taskRouter = require('./routes/tareas.Routes'); 
 
 app.use('/usuarios', userRouter);
 app.use('/tareas', taskRouter);
