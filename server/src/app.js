f4e92179551d806e3d32f61c547a4cf70663ccae
Tomas <tramosyahari@gmal.com>
2024-08-07 19:19:53 -0300

    hice correcciones ahora hay que poner aprueba las rutas

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,27 @@
-const express = requir('express');
+const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
 
-//middlewares
+// Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Ruta para servir archivos estaticos desde la carpeta 'client'
-app.use(express.static(path.join(__dirname, '..', '..', 'client'))) 
+// Ruta para servir archivos estáticos desde la carpeta 'client'
+app.use(express.static(path.join(__dirname, '..', '..', 'client'))); 
 
-//Importar rutas de autenticacion
+// Importar rutas de autenticación
 const authRoutes = require('./routes/authRoutes');
+console.log(authRoutes); // <-- Agregar esto para verificar qué se está importando
 app.use('/auth', authRoutes);
 
-//Ruta ppara servir el archivo about.html por defecto
-app.get('/', (req, res) =>{
-    res.sendFile(path.join(__dirname, '..', '..', 'client', 'about.html'))
+// Ruta para servir el archivo about.html por defecto
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '..', 'client', 'about.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server corriendo en el puerto ${PORT}`)
+    console.log(`Server corriendo en el puerto ${PORT}`);
 });
